d00a29df3adf0ec59b7064982982a0fdc719e184
Abbas <meerabbas110@gmail.com>
2023-07-14 18:39:37 +0530

    exercise on making a liked component

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,17 @@
 //import { useState } from 'react';
 // import { BsFillCalendar2DayFill } from 'react-icons/bs';
 
+import Like from './components/Like';
+
 // import ListGroup from './components/ListGroup';
 // import './App.css';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Like onClick={() => alert('Liked the Post')} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Like.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
+
+interface Props {
+  onClick: () => void;
+}
+
+const Like = ({ onClick }: Props) => {
+  const [liked, setLiked] = useState(true);
+
+  const Toggle = () => {
+    setLiked(!liked);
+    onClick();
+  };
+
+  if (liked) return <AiFillHeart color='red' size={40} onClick={Toggle} />;
+
+  return <AiOutlineHeart size={40} onClick={Toggle} />;
+};
+
+export default Like;

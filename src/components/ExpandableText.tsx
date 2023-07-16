29b1e757a34ca3b04fdd83b71c6a16a9e2234ebe
Abbas <meerabbas110@gmail.com>
2023-07-16 13:22:54 +0530

    added few comments for understanding

--- src/components/ExpandableText.tsx
+++ src/components/ExpandableText.tsx
@@ -3,14 +3,15 @@ interface Props {
   children: string;
   maxChar?: number;
 }
-
+//Substring allow us to cut the string and print it from where we need
 const ExpandableText = ({ children, maxChar = 100 }: Props) => {
   const [isExpanded, setExpended] = useState(false);
 
   if (children.length <= maxChar) return <p>{children}</p>;
   const text = isExpanded ? children : children.substring(0, maxChar);
-
+  //  not using useState for everything just the things that needs to be a dynamic value
   return (
+    // returning the text that we get after substing value
     <p>
       {' '}
       {text}...

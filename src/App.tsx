4aeb689aa9056b7bf3f41e308170e5718be8229d
Abbas <meerabbas110@gmail.com>
2023-07-18 20:23:51 +0530

    delete functionality using axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,73 @@
-import { useEffect } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
-const connect = () => console.log('Connected');
-const disconnecting = () => console.log('Disconnecting');
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
-  useEffect(() => {
-    connect();
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState('');
+  const [isLoading, setLoading] = useState(false);
 
-    return disconnecting();
-  });
-  return <div></div>;
+  useEffect(() => {
+    const controller = new AbortController();
+    setLoading(true);
+    axios
+      .get<User[]>('https://jsonplaceholder.typicode.com/users', {
+        signal: controller.signal,
+      })
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  const deleteUser = (user: User) => {
+    const originalUser = [...users];
+    setUsers(users.filter((u) => u.id !== user.id));
+
+    axios
+      .delete(`https://jsonplaceholder.typicode.com/users/` + user.id)
+      .then((e) => console.log(user.id))
+      .catch((e) => {
+        console.log(e);
+        setUsers(originalUser);
+      });
+  };
+
+  return (
+    <>
+      {isLoading && (
+        <div className='spinner-border'>
+          <p>X</p>
+        </div>
+      )}
+      <ul className='list-group'>
+        {users.map((user) => (
+          <li
+            className='list-group-item d-flex justify-content-between'
+            key={user.id}
+          >
+            {user.name}
+            <button
+              className='btn btn-outline-danger'
+              onClick={() => deleteUser(user)}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
